69b59dee64d6b0e6a5ff05124a89f7004aea54ed
kayki-araujo <64820114+kayki-araujo@users.noreply.github.com>
2021-11-22 09:01:20 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+	<!--
+	Este projeto é uma simples API de troca de mensagens, fornecendo registro de mensagens e retorno de mensagens com banco de dados MySQL.
+	Projeto apresentado como exemplo nas aulas de criação de site do CIMOL
+	  -->
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta charset="UTF-8" />
+		<title>Criação de Sites</title>
+	</head>
+	<body>
+
+		<h1>API Single Chat</h1>
+        <p>Este projeto é uma simples API de troca de mensagens, fornecendo registro de mensagens e retorno de mensagens com banco de dados MySQL.
+	Projeto apresentado como exemplo nas aulas de criação de site do CIMOL</p>
+		<h2>Ler mensagens</h2>
+		<h3>GET</h3>
+		<p>http://localhost/web/chat/</p>
+		<h3>Objeto</h3>
+		<p>{timestamp:159753684}</p>
+
+		<h2>Postar mensagens</h2>
+		<h3>POST</h3>
+		<p>http://localhost/web/chat/</p>
+
+		<h3>Objeto</h3>
+		<p>{nick:'Zico', message:'Olá'}</p>
+
+		<footer>Info Cimol</p>
+	</body>
